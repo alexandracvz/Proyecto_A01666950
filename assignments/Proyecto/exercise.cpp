294848f17b8dd99df386bf8d0743b136b07c79e5
alexandracvz <a01666950@tec.mx>
2026-07-05 19:29:47 +0000

    exercise cpp complete

--- assignments/Proyecto/exercise.cpp
+++ assignments/Proyecto/exercise.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include "Personaje.hpp"
+
 using namespace std;
 
-int main() 
-{
-  cout << "Hello World"<< endl;
-  return 0;
+int main() {
+
+    Personaje personaje1(100, 20, 2);
+    Personaje personaje2(120, 25, 3);
+
+    cout << "===== PERSONAJE 1 =====" << endl;
+    personaje1.imprimir();
+
+    cout << endl;
+
+    cout << "===== PERSONAJE 2 =====" << endl;
+    personaje2.imprimir();
+
+    cout << endl;
+
+    cout << "El personaje 1 ataca al personaje 2." << endl;
+    personaje1.atacar(personaje2);
+
+    cout << endl;
+
+    cout << "===== PERSONAJE 2 DESPUES DEL ATAQUE =====" << endl;
+    personaje2.imprimir();
+
+    return 0;
 }
