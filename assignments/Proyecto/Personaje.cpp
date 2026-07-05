3144be7dabbe17987ed0a1dec10b0279e45e257f
alexandracvz <a01666950@tec.mx>
2026-07-05 19:13:57 +0000

    Initial implementation of personaje class

--- assignments/Proyecto/Personaje.cpp
+++ assignments/Proyecto/Personaje.cpp
@@ -1,1 +1,54 @@
-//GEnera la implementación de la clase base Personaje
+#include "Personaje.hpp"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+Personaje::Personaje() {
+
+    vida = 100;
+    salud = 100;
+    ataque = 20;
+    nivel = 1;
+
+}
+Personaje::Personaje(int vida, int ataque, int nivel) {
+
+    this->vida = vida;
+    salud = vida;
+    this->ataque = ataque;
+    this->nivel = nivel;
+
+}
+
+
+int Personaje::getVida() {
+    return vida;
+}
+
+int Personaje::getSalud() {
+    return salud;
+}
+
+int Personaje::getAtaque() {
+    return ataque;
+}
+
+int Personaje::getNivel() {
+    return nivel;
+}
+void Personaje::setVida(int vida) {
+    this->vida = vida;
+}
+
+void Personaje::setSalud(int salud) {
+    this->salud = salud;
+}
+
+void Personaje::setAtaque(int ataque) {
+    this->ataque = ataque;
+}
+
+void Personaje::setNivel(int nivel) {
+    this->nivel = nivel;
+}
